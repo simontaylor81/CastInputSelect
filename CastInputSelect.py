5c15013ef1149bd055e912a5f3a2f7348b1f2479
Simon Taylor <simontaylor81@hotmail.com>
2018-05-01 11:08:29 +0200

    Added receiver reconnect logic
    Hopefully should be more robust to network outages. Also added a bunch of extra logging, in case of further issues.

--- CastInputSelect.py
+++ CastInputSelect.py
@@ -1,3 +1,4 @@
+import sys
 import pychromecast
 import eiscp
 
@@ -13,25 +14,63 @@ livingroom.wait()
 print "Connected to '{}' ({})".format(livingroom.name, livingroom.host)
 
 
-print "Finding Onkyo receivers..."
-all_receivers = eiscp.eISCP.discover()
-if len(all_receivers) == 0:
-    print "Receiver not found."
-    exit(1)
+receiver = None
+def find_receiver():
+    print "Finding Onkyo receivers..."
+    all_receivers = eiscp.eISCP.discover()
+    if len(all_receivers) == 0:
+        print "Receiver not found."
+        return
 
-receiver = all_receivers[0]
-print "Connected to '{}' ({})".format(receiver.model_name, receiver.host)
+    global receiver
+    receiver = all_receivers[0]
+    print "Connected to '{}' ({})".format(receiver.model_name, receiver.host)
+
+# Find receiver at startup to avoid delay on first connect
+find_receiver()
 
 
-def connected():
-    # Power on receiver and select AUX channel
-    print "Chromecast connected, sending wake up to {}".format(receiver.host)
+# Power on receiver and select AUX channel
+def select_channel():
+    if receiver == None:
+        find_receiver()
+
+    print "Sending channel select to {}".format(receiver.host)
     receiver.command('system-power=on')
     receiver.command('input-selector=aux1')
 
+
+def connected():
+    global receiver
+
+    print "Chromecast connected"
+    try:
+        select_channel()
+        return
+    except:
+        pass
+
+    # Maybe we lost the connection? Try reconnecting.
+    print "Channel select failed, attempting reconnect"
+    try:
+        receiver.disconnect()
+        select_channel()
+        return
+    except:
+        pass
+
+    # Maybe the DHCP lease expired, so now we're on a different IP?
+    print "Reconnect failed, attempting re-discovery"
+    try:
+        receiver = None
+        select_channel()
+    except:
+        pass
+
+
 def disconnected():
     # Don't do anything on disconnection
-    pass
+    print "ChromeCast disconnected"
 
 
 def is_casting(media_status):
@@ -45,13 +84,20 @@ class Listener:
         cast.register_status_listener(self)
 
     def new_cast_status(self, status):
-        new_is_connected = is_connected(status)
-        if not self.curr_is_connected and new_is_connected:
-            connected()
-        elif self.curr_is_connected and not new_is_connected:
-            disconnected()
-        
-        self.curr_is_connected = new_is_connected
+        try:
+            new_is_connected = is_connected(status)
+            print "new_cast_status:"
+            print "  " + str(status)
+            print "  self.curr_is_connected = {}".format(self.curr_is_connected)
+            print "  new_is_connected = {}".format(new_is_connected)
+            if not self.curr_is_connected and new_is_connected:
+                connected()
+            elif self.curr_is_connected and not new_is_connected:
+                disconnected()
+            
+            self.curr_is_connected = new_is_connected
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
 
 listener = Listener(livingroom)
 

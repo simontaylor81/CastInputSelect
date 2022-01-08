e79a412b978a244640ad9ee77a40fefcd74ed108
Simon Taylor <simontaylor81@hotmail.com>
2022-01-08 22:29:49 +0100

    Initial python3 port

--- CastInputSelect.py
+++ CastInputSelect.py
@@ -2,29 +2,42 @@ import sys
 import pychromecast
 import eiscp
 
-print "Finding Chromecasts..."
-all_casts = pychromecast.get_chromecasts()
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("zeroconf").setLevel(logging.DEBUG)
 
-livingroom = next((x for x in all_casts if x.device.friendly_name == 'Living Room'), None)
-if livingroom == None:
-    print "Living Room Chromecast not found."
+known_hosts = None
+if len(sys.argv) > 1:
+    known_hosts = [sys.argv[1]]
+
+print("Finding Chromecasts...")
+casts, browser = pychromecast.get_listed_chromecasts(friendly_names=["Living Room"], known_hosts=known_hosts)
+#print(casts)
+browser.stop_discovery()
+
+#livingroom = next((x for x in all_casts if x.cast_info.friendly_name == 'Living Room'), None)
+#if livingroom == None:
+if len(casts) == 0:
+    print("Living Room Chromecast not found.")
     exit(1)
+livingroom = casts[0]
 
 livingroom.wait()
-print "Connected to '{}' ({})".format(livingroom.name, livingroom.host)
+lr_info = livingroom.cast_info
+print("Connected to {} '{}' ({})".format(lr_info.model_name, lr_info.friendly_name, lr_info.host))
 
 
 receiver = None
 def find_receiver():
-    print "Finding Onkyo receivers..."
+    print("Finding Onkyo receivers...")
     all_receivers = eiscp.eISCP.discover()
     if len(all_receivers) == 0:
-        print "Receiver not found."
+        print("Receiver not found.")
         return
 
     global receiver
     receiver = all_receivers[0]
-    print "Connected to '{}' ({})".format(receiver.model_name, receiver.host)
+    print("Connected to '{}' ({})".format(receiver.model_name, receiver.host))
 
 # Find receiver at startup to avoid delay on first connect
 find_receiver()
@@ -35,7 +48,7 @@ def select_channel():
     if receiver == None:
         find_receiver()
 
-    print "Sending channel select to {}".format(receiver.host)
+    print("Sending channel select to {}".format(receiver.host))
     receiver.command('system-power=on')
     receiver.command('input-selector=aux1')
 
@@ -43,7 +56,7 @@ def select_channel():
 def connected():
     global receiver
 
-    print "Chromecast connected"
+    print("Chromecast connected")
     try:
         select_channel()
         return
@@ -51,7 +64,7 @@ def connected():
         pass
 
     # Maybe we lost the connection? Try reconnecting.
-    print "Channel select failed, attempting reconnect"
+    print("Channel select failed, attempting reconnect")
     try:
         receiver.disconnect()
         select_channel()
@@ -60,7 +73,7 @@ def connected():
         pass
 
     # Maybe the DHCP lease expired, so now we're on a different IP?
-    print "Reconnect failed, attempting re-discovery"
+    print("Reconnect failed, attempting re-discovery")
     try:
         receiver = None
         select_channel()
@@ -70,7 +83,7 @@ def connected():
 
 def disconnected():
     # Don't do anything on disconnection
-    print "ChromeCast disconnected"
+    print("ChromeCast disconnected")
 
 
 def is_casting(media_status):
@@ -86,10 +99,10 @@ class Listener:
     def new_cast_status(self, status):
         try:
             new_is_connected = is_connected(status)
-            print "new_cast_status:"
-            print "  " + str(status)
-            print "  self.curr_is_connected = {}".format(self.curr_is_connected)
-            print "  new_is_connected = {}".format(new_is_connected)
+            print("new_cast_status:")
+            print("  " + str(status))
+            print("  self.curr_is_connected = {}".format(self.curr_is_connected))
+            print("  new_is_connected = {}".format(new_is_connected))
             if not self.curr_is_connected and new_is_connected:
                 connected()
             elif self.curr_is_connected and not new_is_connected:
@@ -97,9 +110,9 @@ class Listener:
             
             self.curr_is_connected = new_is_connected
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print("Unexpected error:", sys.exc_info()[0])
 
 listener = Listener(livingroom)
 
-print "Waiting for connections..."
+print("Waiting for connections...")
 livingroom.socket_client.join()
